3aef8a7827aa0309c71f49149eabf6b428b69735
yangsongbai1 <yangsongbai1@jd.com>
2024-10-19 18:54:38 +0800

    log

--- plain/src/main/java/org/elasticsearch/apack/plain/search/agg/Aggs.java
+++
@@ -1,6 +0,0 @@
-package org.elasticsearch.apack.plain.search.agg;
-
-public class Aggs {
-
-    public static final String ROOT_GROUP_NAME = "groupby";
-}
--- plain/src/main/java/org/elasticsearch/apack/plain/search/extractor/AggsExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.elasticsearch.apack.plain.search.extractor;
-
-import org.elasticsearch.common.io.stream.NamedWriteable;
-
-public interface AggsExtractor<T> extends NamedWriteable {
-
-    Object extract(T response);
-
-}
